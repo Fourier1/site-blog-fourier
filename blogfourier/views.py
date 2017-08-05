1b1296e371cc2a21cc05541a99b998525b653516
fourierblog <fouriersaint@gmail.com>
2017-08-05 11:34:40 +0000

    modificztion du template et affichier les information de la bd dans le naviagateur

--- blogfourier/views.py
+++ blogfourier/views.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
+from django.utils import timezone
+from .models import blogs
 
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, 'blogfourier/post_list.html', {})
+    # creation de notre queryset , qui servira a afficher la date de publication (posts)
+    posts = blogs.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blogfourier/post_list.html', {'posts': posts})
